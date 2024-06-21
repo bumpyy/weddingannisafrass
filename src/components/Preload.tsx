54b4f116c261eeba0bf6365c6422f3d48af07958
Ghassan Fs <fsghassan2429d@gmail.com>
2024-06-21 13:49:50 +0700

    update preloader

--- src/components/Preload.tsx
+++ src/components/Preload.tsx
@@ -34,7 +34,7 @@ export default function Preload({
         <StarIcon />
       </div>
 
-      <div className="relative h-full">
+      <div className="relative h-dvh">
         <img
           className="object-bottom brightness-75 saturate-50"
           src="assets/images/preload.png"
